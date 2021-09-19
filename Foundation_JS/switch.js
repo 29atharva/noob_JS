9a17b3e635820503808aa1f0ac079415c4591065
29atharva <29atharva@gmail.com>
2021-09-19 22:42:09 +0530

    Create switch.js

---
+++ Foundation_JS/switch.js
@@ -0,0 +1,23 @@
+//basic javaScript program.
+
+let val = 1;
+let answer = "";
+
+switch (val){
+case 1 :
+case 'A':
+case 'a':
+answer = "alpha";
+break;
+case 2 :
+case 'A':
+case 'a':
+answer : "alpha";
+break;
+
+case 1 :
+case 'A':
+case 'a':
+answer = "alpha";
+break;
+}
