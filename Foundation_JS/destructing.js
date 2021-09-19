a313e3212e90aa8a8460f353390487156c865242
29atharva <29atharva@gmail.com>
2021-09-19 23:23:19 +0530

    Create destructing.js

---
+++ Foundation_JS/destructing.js
@@ -0,0 +1,34 @@
+const a= {v:"atharva",obj:{v:"kavishwar"}}
+
+const { obj:u,v:g } = {v:"atharva",obj:{v:"kavishwar"}}
+
+// const u = a.obj
+// const g =a.v
+
+const r={k:{j:{i:{o:{n:0}}}}}
+const {k:{j:{i:{o:{n}}}}}=r
+// const n= k.j.i.o.n
+
+// JSON.parse("{}") -> object
+// Object.new() -> object
+
+console.log(Object.assign({a:0},{b:1},{c:"anhul"}))
+ delete a.v
+
+ console.log(a)
+
+
+
+
+// obj,g
+
+// const name = a.obj.name
+
+
+// const name= {name:"atharva"}.name
+
+// console.log(v,v)
+
+
+
+// console.log(name,foo) 
